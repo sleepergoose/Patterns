c654eab5bc460ee07832c30ca2f81ac2b1e4515a
sleepergoose <sleepergoose@gmail.com>
2021-04-03 17:08:56 +0300

    Add project files.

---
+++ Patterns/Factory.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Patterns
+{
+    /// <summary>
+    /// Common interface for all vehicles
+    /// </summary>
+    public interface IVehicle
+    {
+        string PetName { get; set; }
+        string Manufacturer { get; set; }
+        void Move();
+    }
+
+    /// <summary>
+    /// Several types of different vehicles which implement IVehicle
+    /// </summary>
+    public class Car : IVehicle
+    {
+        public string PetName { get; set; }
+        public string Manufacturer { get; set; }
+
+        public void Move() => 
+            Console.WriteLine($"Car {PetName} is moving");
+    }
+
+    public class Ship : IVehicle
+    {
+        public string PetName { get; set; }
+        public string Manufacturer { get; set; }
+
+        public void Move() =>
+            Console.WriteLine($"Ship {PetName} is sailing");
+    }
+
+    public class Plane : IVehicle
+    {
+        public string PetName { get; set; }
+        public string Manufacturer { get; set; }
+
+        public void Move() =>
+            Console.WriteLine($"Plane {PetName} is flying");
+    }
+
+    /// <summary>
+    /// Vehicle Factory will be creating of different types of vehicles
+    /// </summary>
+    public class VehicleFactory
+    {
+        private int CarCounter = 0;
+        private int ShipCounter = 0;
+        private int PlaneCounter = 0;
+
+        public IVehicle GetVehicle(VehicleType vehicleType, string petName = null, string manufacturer = null)
+        {
+            switch (vehicleType)
+            {
+                case VehicleType.Car:
+                    CarCounter++;
+                    return new Car() { PetName = petName == null ? "Beauty" : petName, Manufacturer = manufacturer == null ? "BMW" : manufacturer };
+                case VehicleType.Plane:
+                    PlaneCounter++;
+                    return new Plane() { PetName = petName == null ? "Jet" : petName, Manufacturer = manufacturer == null ? "Boing" : manufacturer };
+                case VehicleType.Ship:
+                    ShipCounter++;
+                    return new Ship() { PetName = petName == null ? "Titanic" : petName, Manufacturer = manufacturer == null ? "Cat" : manufacturer };
+                default:
+                    throw new ApplicationException("There is no such a vehicle type");
+            }
+        }
+
+        public void PrintStatistic()
+        {
+            Console.WriteLine($"Cars: {CarCounter}; Planes: {PlaneCounter}; Ship {ShipCounter}");
+        }
+    }
+
+    /// <summary>
+    /// Enum of vehicle types
+    /// </summary>
+    public enum VehicleType
+    {
+        Car,
+        Plane,
+        Ship
+    }
+}
+
---
+++ Patterns/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Patterns
+{
+    class Program
+    {
+        static Action<object> Write = (data) => Console.WriteLine(data);
+        static void Main(string[] args)
+        {
+            FactoryTest();
+        }
+
+        static void FactoryTest()
+        {
+            VehicleFactory factory = new VehicleFactory();
+
+            IVehicle[] vehicles = new IVehicle[3];
+
+            vehicles[0] = factory.GetVehicle(VehicleType.Car);
+            vehicles[1] = factory.GetVehicle(VehicleType.Ship, petName: "Brave");
+            vehicles[2] = factory.GetVehicle(VehicleType.Plane, petName: "Boing 737");
+
+            foreach (var item in vehicles)
+                item.Move();
+
+            factory.PrintStatistic();
+        }
+    }
+}
